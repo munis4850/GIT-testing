7cf38e16236d0f76674c2c438b0493e6719cdb14
Modestas <109092499+munis4850@users.noreply.github.com>
2022-12-13 19:06:33 +0200

    update

--- file_to_load.php
+++ file_to_load.php
@@ -5,8 +5,19 @@ declare(strict_types=1);
 1. Parašykite funkciją 'dividesBy5', kuri priimtų int tipo skaičių ir grąžintų jo dalybos iš 5 liekaną.
 */
 
-function dividesBy5 (int $firstNumber, int $secondNumber ): int
-{
-    return ($firstNumber % $secondNumber);
-}
-var_dump(dividesBy5(7,2));
+//function dividesBy5 (int $firstNumber, int $secondNumber ): int
+//{
+//    return ($firstNumber % $secondNumber);
+//}
+//var_dump(dividesBy5(7,2));
+
+/*
+2. Parašykite funkciją 'arrayPrinter', kuri priimtų array tipo parametrą ir
+išspausdintų kiekvieną masyvo elementą naujoje eilutėje.
+Funkcijos kvietimas: arrayPrinter(['some text', 'another text'])
+Funkcija grąžina: funkcija nieko negrąžina - ji tik išspausdina masyvą į terminalą:
+'some text'
+'another text'
+...
+*/
+function arrayPrinter (array %arejus, )
