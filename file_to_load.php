2e898bdc217714633d299459e7b91635e106f4dc
Modestas <109092499+munis4850@users.noreply.github.com>
2022-12-13 19:04:53 +0200

    naujas

--- file_to_load.php
+++ file_to_load.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+/*
+1. Parašykite funkciją 'dividesBy5', kuri priimtų int tipo skaičių ir grąžintų jo dalybos iš 5 liekaną.
+*/
 
-//komentuosim del gito gggggg
-echo "bandom";
+function dividesBy5 (int $firstNumber, int $secondNumber ): int
+{
+    return ($firstNumber % $secondNumber);
+}
+var_dump(dividesBy5(7,2));
