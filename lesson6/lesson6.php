2fbdeeda372b07baa3d88b5fa1ee26b9a32ccca7
Modestas <109092499+munis4850@users.noreply.github.com>
2022-12-14 20:06:43 +0200

    3ia ok

--- lesson6/lesson6.php
+++ lesson6/lesson6.php
@@ -1,13 +1,17 @@
 <?php
-
-
 declare(strict_types=1);
 
-
 /*
-Pasinaudodami masyvo operatoriumi paimkite elementą, kurio reikšmė yra 3 ir grąžinkite tą reikšmę iš funkcijos.
-*/
-
-$numbers = [0, 1, 2, 3, 4];
-
-var_dump($numbers);
+   Pasinaudodami masyvo operatoriumi paimkite elementą, kurio reikšmė yra 99 ir grąžinkite tą reikšmę iš funkcijos.
+   */
+$numbers = [
+    [0, 1],
+    [1, 0, 2],
+    [
+        0,
+        [
+            0, 1, 99
+        ],
+    ],
+];
+var_dump($numbers[2][1][2]);
