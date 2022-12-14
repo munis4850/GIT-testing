573f7d5a30340f646fb5caed51ea4ca1267790e3
Modestas <109092499+munis4850@users.noreply.github.com>
2022-12-14 19:36:19 +0200

    testing 1exercise

---
+++ lesson6/lesson6.php
@@ -0,0 +1,13 @@
+<?php
+
+
+declare(strict_types=1);
+
+
+/*
+Pasinaudodami masyvo operatoriumi paimkite elementą, kurio reikšmė yra 3 ir grąžinkite tą reikšmę iš funkcijos.
+*/
+
+$numbers = [0, 1, 2, 3, 4];
+
+var_dump($numbers);
